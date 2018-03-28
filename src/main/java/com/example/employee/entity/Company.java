dba1b17d74a5bcb2977762a9913f2e461668ad86
Pei Zhang <pezhang@thoughtworks.com>
2018-03-28 15:42:23 +0800

    modify desc

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,44 +1,5 @@
 package com.example.employee.entity;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.List;
-
-@Entity
 public class Company {
-    @Id
-    @GeneratedValue
-    private Integer id;
 
-    @NotNull
-    @Column(name = "companyName")
-    private String companyName;
-
-    @NotNull
-    @Column(name = "employeesNumber")
-    private Integer employeesNumber;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public Integer getEmployeesNumber() {
-        return employeesNumber;
-    }
-
-    public void setEmployeesNumber(Integer employeesNumber) {
-        this.employeesNumber = employeesNumber;
-    }
 }
